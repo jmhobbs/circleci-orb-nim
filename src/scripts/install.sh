2094962785dd6fc260d07a785e07b86ad5b67280
John Hobbs <john.hobbs@getflywheel.com>
2020-11-01 00:54:23 -0500

    Not a gzip

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,6 +1,6 @@
 Install() {
   wget https://nim-lang.org/download/nim-1.4.0-linux_x64.tar.xz
-  tar -zxf nim-1.4.0-linux_x64.tar.xz
+  tar -xf nim-1.4.0-linux_x64.tar.xz
   cd nim-1.4.0 || return
   bash ./install.sh /usr/local/bin
 }
