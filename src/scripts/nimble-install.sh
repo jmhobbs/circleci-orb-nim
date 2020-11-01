f8e021c789b5f284f26e2e0edf806c3740493c40
John Hobbs <john.hobbs@getflywheel.com>
2020-10-31 22:34:36 -0500

    Nimble install command.

---
+++ src/scripts/nimble-install.sh
@@ -0,0 +1,10 @@
+NimbleInstall() {
+    nimble install "${PARAM_PACKAGE}"
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+  NimbleInstall
+fi
